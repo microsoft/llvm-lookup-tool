872d715badc0bd805f804a73d6d31a85cff0f1a3
Adi Azulay <adazulay@microsoft.com>
2020-04-22 08:47:35 -0700

    working needs documentation

--- get_llvm.py
+++ get_llvm.py
@@ -12,36 +12,99 @@
 # MIT License terms detialed in LICENSE
 
 import json
+import re
+import argparse
 
-found_flag = False
+# The PyInquirer module is used for creating a selectable list. This is a nice
+# feature if you're using this in command line, but not needed if this is
+# implemented in another application.
+try:
+    from PyInquirer import style_from_dict, Token, prompt, Separator
+    inquirer = True
+except ImportError:
+    inquirer = False
+    pass
 
-board_name = input("Enter board name: ")
+
+parser = argparse.ArgumentParser(
+    description='Get LLVM compiler option for common boards.')
+parser.add_argument('--list', action='store_true',
+                    help='Print a list of availble boards')
+parser.add_argument('--search', metavar='keyword', type=str,
+                    help='Search for board by keyword (case sensative)')
+parser.add_argument('--get', '-g', metavar='board name', type=str,
+                    help='Enter board name to get the LLVM compiler arguments')
+parser.add_argument('--list_select', '-lss', action='store_true')
+args = parser.parse_args()
+
+
+# board_name = input("Enter board name: ")
 
 with open('boards.json') as f:
     data = json.load(f)
 
-# TODO: better integration of the list command
-if board_name == 'list':
+
+def list_boards():
     for i in data['Boards']:
         print(i['board'])
-    board_name = input("Enter board name: ")
+    get_board(input("Enter board name: "))
 
-for i in data['Boards']:
-    if board_name == i['board']:
-        found_flag = True
-        output = ''
-        if 'llvm-triple' in i:
-            output += '-mtriple={}'.format(i['llvm-triple'])
-        if 'cpu' in i:
-            output += ' -mcpu={}'.format(i['cpu'])
-        if 'relocation-model' in i:
-            output += ' -relocation-model{}'.format(i['relocation-model'])
-        if 'floating-point' in i:
-            output += ' -float-abi={}'.format(i['floating-point'])
-        if 'features' in i:
-            output += ' -mattr={}'.format(i['features'])
-        print(output)
-        break
 
-if found_flag is False:
-    print('Board not found')
+def list_select():
+    if inquirer is not False:
+        options = []
+        for i in data['Boards']:
+            options.append(i['board'])
+        print(options)
+        question = [{
+            'type': 'list',
+            'name': 'board',
+            'message': 'select board',
+            'choices': options
+        }
+
+        ]
+        answers = prompt(question)
+        return(answers['board'])
+    else:
+        print('install PyInquirer for this option')
+        exit()
+
+
+def search(keyword):
+    for i in data['Boards']:
+        if re.match(keyword, i['board']):
+            print(i['board'])
+
+
+def get_board(board_name):
+    found_flag = False
+    for i in data['Boards']:
+        if board_name == i['board']:
+            found_flag = True
+            output = ''
+            if 'llvm-triple' in i:
+                output += '-mtriple={}'.format(i['llvm-triple'])
+            if 'cpu' in i:
+                output += ' -mcpu={}'.format(i['cpu'])
+            if 'relocation-model' in i:
+                output += ' -relocation-model{}'.format(i['relocation-model'])
+            if 'floating-point' in i:
+                output += ' -float-abi={}'.format(i['floating-point'])
+            if 'features' in i:
+                output += ' -mattr={}'.format(i['features'])
+            print(output)
+            break
+    if found_flag is False:
+        print('Board not found')
+
+
+if __name__ == '__main__':
+    if args.list:
+        list_boards()
+    if args.search:
+        search(args.search)
+    if args.get:
+        get_board(args.get)
+    if args.list_select:
+        get_board(list_select())
